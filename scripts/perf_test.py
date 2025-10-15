88dc149ad822eb16e88ccef60f3459270466662e
Lukin Kratas <kratochvila.lukin@gmail.com>
2025-10-15 22:52:27 +0200

    fix types

--- scripts/perf_test.py
+++ scripts/perf_test.py
@@ -1,12 +1,12 @@
 import asyncio
+import logging
 
 from curl_cffi import requests
+from logging_config import setup_logging
 from yfinance import Ticker
 
 from yafin import Stonk
-from yafin.utils import track_performance, process_chart_like_yfinance
-import logging
-from logging_config import setup_logging
+from yafin.utils import process_chart_like_yfinance, track_performance
 
 logger = logging.getLogger(__name__)
 yfinance_logger = logging.getLogger('yfinance')
@@ -14,9 +14,9 @@ yfinance_logger.setLevel(logging.ERROR)
 
 NRUNS = 10
 
+
 @track_performance(NRUNS)
 def main_yfinance() -> None:  # noqa: D103
-
     # async session does not work, throws
     #   yfinance.exceptions.YFDataException: Yahoo API requires curl_cffi session
     #   not <class 'curl_cffi.requests.session.AsyncSession'>.
@@ -28,10 +28,11 @@ def main_yfinance() -> None:  # noqa: D103
 
     history_df = meta.history(period='1y', interval='1d')
 
-    # print(history_df)
-    # print(history_df.info())
+    print(history_df)
+    print(history_df.info())
 
     session.close()
+
 
 @track_performance(NRUNS)
 async def main() -> None:  # noqa: D103
@@ -40,8 +41,9 @@ async def main() -> None:  # noqa: D103
     chart = await stonk.get_chart(period_range='1y', interval='1d')
     chart_df = process_chart_like_yfinance(chart)
 
-    # print(chart_df)
-    # print(chart_df.info())
+    print(chart_df)
+    print(chart_df.info())
+
 
 if __name__ == '__main__':
     setup_logging()
