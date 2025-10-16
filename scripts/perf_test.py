d905d278e464672a4f559cf7160aefccba3afddd
Lukin Kratas <kratochvila.lukin@gmail.com>
2025-10-16 08:18:24 +0200

    move track_performance decorator to scripts

--- scripts/perf_test.py
+++ scripts/perf_test.py
@@ -1,18 +1,65 @@
 import asyncio
 import logging
+import statistics
+from collections.abc import Callable
+from datetime import timedelta
+from functools import wraps
+from time import perf_counter
+from typing import Any
 
 from curl_cffi import requests
 from logging_config import setup_logging
 from yfinance import Ticker
 
 from yafin import Stonk
-from yafin.utils import process_chart_like_yfinance, track_performance
+from yafin.utils import _get_func_name_and_args, process_chart_like_yfinance
 
 logger = logging.getLogger(__name__)
 yfinance_logger = logging.getLogger('yfinance')
 yfinance_logger.setLevel(logging.ERROR)
 
 NRUNS = 10
+
+
+def track_performance(n: int = 1) -> Callable[..., Any]:
+    """Decorator for logging functions and its' performance."""
+
+    def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            func_name, _ = _get_func_name_and_args(func, args)
+            run_times = []
+
+            print(f'{func_name} running {n} time(s) started.')
+            total_start_time = perf_counter()
+
+            for idx in range(1, n + 1):
+                logger.debug(f'  {func_name} run no.{idx} started.')
+                run_start_time = perf_counter()
+
+                result = func(*args, **kwargs)
+
+                run_elapsed_time = perf_counter() - run_start_time
+                run_times.append(run_elapsed_time)
+                run_elapsed_time_td = timedelta(seconds=run_elapsed_time)
+                logger.debug(
+                    f'  {func_name} run no.{idx} finished with '
+                    f'elapsed_time={run_elapsed_time_td}.'
+                )
+
+            total_elapsed_time = perf_counter() - total_start_time
+            total_elapsed_time_td = timedelta(seconds=total_elapsed_time)
+            avg_elapsed_time_td = timedelta(seconds=statistics.mean(run_times))
+            logger.debug(
+                f'{func_name} running {n} time(s) finished with '
+                f'total={total_elapsed_time_td} and average={avg_elapsed_time_td}.'
+            )
+
+            return result
+
+        return wrapper
+
+    return decorator
 
 
 @track_performance(NRUNS)
--- src/yafin/utils.py
+++ src/yafin/utils.py
@@ -1,9 +1,6 @@
 import logging
-import statistics
 from collections.abc import Callable
-from datetime import timedelta
 from functools import wraps
-from time import perf_counter
 from typing import Any, Type
 
 import pandas as pd
@@ -107,47 +104,6 @@ def track_args(func: Callable[..., Any]) -> Callable[..., Any]:
         return result
 
     return async_wrapper
-
-
-def track_performance(n: int = 1) -> Callable[..., Any]:
-    """Decorator for logging functions and its' performance."""
-
-    def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
-        @wraps(func)
-        def wrapper(*args: Any, **kwargs: Any) -> Any:
-            func_name, _ = _get_func_name_and_args(func, args)
-            run_times = []
-
-            print(f'{func_name} running {n} time(s) started.')
-            total_start_time = perf_counter()
-
-            for idx in range(1, n + 1):
-                logger.debug(f'  {func_name} run no.{idx} started.')
-                run_start_time = perf_counter()
-
-                result = func(*args, **kwargs)
-
-                run_elapsed_time = perf_counter() - run_start_time
-                run_times.append(run_elapsed_time)
-                run_elapsed_time_td = timedelta(seconds=run_elapsed_time)
-                logger.debug(
-                    f'  {func_name} run no.{idx} finished with '
-                    f'elapsed_time={run_elapsed_time_td}.'
-                )
-
-            total_elapsed_time = perf_counter() - total_start_time
-            total_elapsed_time_td = timedelta(seconds=total_elapsed_time)
-            avg_elapsed_time_td = timedelta(seconds=statistics.mean(run_times))
-            logger.debug(
-                f'{func_name} running {n} time(s) finished with '
-                f'total={total_elapsed_time_td} and average={avg_elapsed_time_td}.'
-            )
-
-            return result
-
-        return wrapper
-
-    return decorator
 
 
 def process_chart_like_yfinance(chart: dict[str, Any]) -> pd.DataFrame:
